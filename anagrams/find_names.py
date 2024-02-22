9ff238fa1e175d9d8aa71c2a8b0ff8b7b5767245
salimfadhley <salimfadhley@gmail.com>
2024-02-22 19:11:33 +0000

    Improved presentation

--- anagrams/find_names.py
+++ anagrams/find_names.py
@@ -1,5 +1,6 @@
 import itertools
 import logging
+import os
 
 from anagrams.config import SOURCE_TEXT
 from anagrams.count_letters import cl, CountLettersResultStructure, compliment_count, InvalidCompliment
@@ -11,21 +12,26 @@ def main():
     source_chars:CountLettersResultStructure = cl(SOURCE_TEXT)
     names_struct:NamesStructure = get_names_structure()
 
-    with open("anagram_names.txt", "w") as output_file:
-        for k,names in names_struct.items():
-            try:
-                k_complement = compliment_count(source_chars,k)
-            except InvalidCompliment as ve:
-                # log.info(f"No complement of {next(iter(names))}")
-                continue
+    found_names = []
 
-            if k_complement in names_struct:
-                last_names = names_struct[k_complement]
-                for name in names:
-                    for last_name in last_names:
-                        full_name = f"{name} {last_name}"
-                        log.info(full_name)
-                output_file.write(f"{full_name}\n")
+    for k,names in names_struct.items():
+        try:
+            k_complement = compliment_count(source_chars,k)
+        except InvalidCompliment as ve:
+            # log.info(f"No complement of {next(iter(names))}")
+            continue
+
+        if k_complement in names_struct:
+            last_names = names_struct[k_complement]
+            for name in names:
+                for last_name in last_names:
+                    full_name = f"{name} {last_name}"
+                    log.info(full_name)
+                    found_names.append(full_name)
+
+
+    with open("anagram_names.txt", "w") as output_file:
+        output_file.writelines(f"{n}{os.linesep}" for n in  sorted(found_names))
 
 
 
